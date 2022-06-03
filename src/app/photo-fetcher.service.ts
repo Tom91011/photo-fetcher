c705534cf05a9a8414132f5e6f57076468bb98c8
tom91011 <tomnightingale@hotmail.com>
2022-06-03 12:44:06 +0100

    Make api request for random photo using headers

--- src/app/photo-fetcher.service.ts
+++ src/app/photo-fetcher.service.ts
@@ -7,4 +7,13 @@ import { HttpClient } from '@angular/common/http';
 export class PhotoFetcherService {
 
   constructor(private http: HttpClient) { }
+
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 
+        'client-ID ZxKOBom24JCOKoFv08sW_tmnciZQnOWW00wRU4V97Ls'
+      }
+    })
+  }  
 }
